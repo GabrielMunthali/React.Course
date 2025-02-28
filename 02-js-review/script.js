490b09ad6c5c544bb1dc22f8fbae75b0d674a181
Gabriel Munthali <gabmunthali@gmail.com>
2025-02-28 10:03:29 +0200

    26. The Array filter method

--- 02-js-review/script.js
+++ 02-js-review/script.js
@@ -66,7 +66,7 @@ const data = [
 		publicationDate: "1965-01-01",
 		author: "Frank Herbert",
 		genres: ["science fiction", "novel", "adventure"],
-		hasMovieAdaptation: true,
+		hasMovieAdaptation: false,
 		pages: 658,
 		translations: {
 			spanish: "",
@@ -247,3 +247,14 @@ const esssentialData =  books.map((book) => ({
 	reviewsCount: getTotalReviewCount(book),
 }));
 esssentialData;
+
+ const longBooksWithMovie = books
+ 	.filter((book) => book.pages > 500)
+	.filter((book) => book.hasMovieAdaptation);
+ longBooksWithMovie;
+
+ const adventureBooks = books.filter((books) => 
+	books.genres.includes("adventure")
+).map((book) => book.title);
+
+adventureBooks;
