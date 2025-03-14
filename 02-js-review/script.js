7942b110ab38fbd5dd7915a244c41b2f320f167b
Gabriel Munthali <gabmunthali@gmail.com>
2025-03-14 11:02:54 +0200

    30. Asynchronous Javascript: Async/Awaiit

--- 02-js-review/script.js
+++ 02-js-review/script.js
@@ -224,7 +224,7 @@ function getTotalReviewCount(book){
 console.log(getTotalReviewCount(book));
 */
 
-
+/*
 function getTotalReviewCount(book) {
 	const goodreads = book.reviews.goodreads.reviewsCount;
 	const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
@@ -285,3 +285,13 @@ booksAfterDelete;
 //3) Update a boo object in the array
 const booksAfterUpdate = booksAfterDelete.map((book) => book.id === 1 ? { ...book, pages:1210 } : book);
 booksAfterUpdate;
+*/
+
+
+
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+.then((res) => res.json())
+.then((data) => console.log(data));
+
+console.log("jonas");
