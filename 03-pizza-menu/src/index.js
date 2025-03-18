8a2267f7591c2af4f71860a7884a044fa78b51f4
Gabriel Munthali <gabmunthali@gmail.com>
2025-03-18 19:22:14 +0200

    41. passing and receiving props

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -72,12 +72,33 @@ function Menu(){
 	return (
 		<main className="menu">
 			<h2>Our Menu</h2>
-			<Pizza/>
-			<Pizza/>
-			<Pizza/>
-			<Pizza/>
+			<Pizza
+				name="Pizza Spinaci"
+				ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+				photoName="pizzas/spinaci.jpg"
+				price={10}
+			/>
+
+			<Pizza
+				name="Pizza Funghi"
+				ingredients="Tomato, mushrooms"
+				price={12}
+				photoName="pizzas/funghi.jpg"
+			/>
 		</main>
 	);
+}
+function Pizza(props){
+  return(
+	<div className="pizza">
+		<img src={props.photoName} alt={props.name}/>
+		<div>
+			<h3>{props.name}</h3> 
+			<p>{props.ingredients}</p>
+			<span>{props.price}</span>
+		</div>
+	</div>
+  );
 }
 
 function Footer(){
@@ -99,15 +120,6 @@ function Footer(){
 }
 
 
-function Pizza(){
-  return(
-	<div>
-		<img src="pizzas/spinaci.jpg" alt="Pizza Spinachi"/>
-		<h3> Pizza Spinaci </h3> 
-		<p> Tomato, mozarella, spinach, and ricotta cheese </p>
-	</div>
-  );
-}
 
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
