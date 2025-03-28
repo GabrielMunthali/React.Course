19fbeb92cc5c7fb7412a8b450e8a8932474306e2
Gabriel Munthali <gabmunthali@gmail.com>
2025-03-28 11:09:38 +0200

    63. Adding another piece of state

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -8,7 +8,7 @@ const messages = [
 
 export default function App() {
 	const [step, setStep] = useState(1);
-	const [test, setTest] = useState({name: "Gabe"});
+	const [isOpen, setIsOpen] = useState(true);
 
 	function handlePrevious() {
 		if (step > 1) setStep(step-1);
@@ -16,32 +16,38 @@ export default function App() {
 
 	function handleNext() {
 		if (step < 3) setStep(step + 1);
-
-		setTest({name: "Riel"});
 	}
 
-	return <div className="steps">
-				<div className="numbers">
-					<div className={step>=1 ? "active" : ""}>1</div>
-					<div className={step>=2 ? "active" : ""}>2</div>
-					<div className={step>=3 ? "active" : ""}>3</div>
-				</div>
+	return (
+		<>
+			<button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+			{isOpen && (
+				<div className="steps">
+					<div className="numbers">
+						<div className={step>=1 ? "active" : ""}>1</div>
+						<div className={step>=2 ? "active" : ""}>2</div>
+						<div className={step>=3 ? "active" : ""}>3</div>
+					</div>
 
-				<p className="message">Step {step}: {messages[step - 1]} {test.name}</p>
+					<p className="message">Step {step}: {messages[step - 1]}</p>
 
-				<div className="buttons">
-					<button 
-						style={{backgroundColor: "#7950F2", color: "#FFF"}} 
-						onClick={handlePrevious}
-						>
-						Previous
-					</button>
-					<button 
-						style={{backgroundColor: "#7950F2", color: "#FFF"}}
-						onClick={handleNext}
-						>
-						Next
-					</button>
+					<div className="buttons">
+						<button 
+							style={{backgroundColor: "#7950F2", color: "#FFF"}} 
+							onClick={handlePrevious}
+							>
+							Previous
+						</button>
+						<button 
+							style={{backgroundColor: "#7950F2", color: "#FFF"}}
+							onClick={handleNext}
+							>
+							Next
+						</button>
+					</div>
 				</div>
-			</div>;
+			)}
+		</>
+	);
+
 }
