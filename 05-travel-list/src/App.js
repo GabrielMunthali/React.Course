52df57bae6d63dd84a7f365c68aa0b3bd8b60879
Gabriel Munthali <gabmunthali@gmail.com>
2025-03-31 17:26:15 +0200

    73. Controlled elements

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [{
 		id: 1,
 		description: "Passports",
@@ -11,7 +13,7 @@ const initialItems = [{
 		packed: true
 	},
 	{
-		id: 2,
+		id: 3,
 		description: "Charger",
 		quantity: 1,
 		packed: false
@@ -35,19 +37,30 @@ function Logo() {
 
 function Form() {
 
+	const [description, setDescription] = useState("");
+	const [quantity, setQuantity] = useState(5);
+
 	function handleSubmit(e) {
 		e.preventDefault();
+
+		if (!description) return;
+
+		const newItem = {description, quantity, packed: false, id: Date.now()};
+		console.log(newItem);
+
+		setDescription("");
+		setQuantity(1);
 	}
 
 	return (
 		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for your 😍 trip?</h3>
-			<select>
+			<select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
 				{Array.from({length:20}, (_,i) => i + 1).map((num) => (
 					<option value={num} key={num}>{num}</option>
 				))}
 			</select>
-			<input type="text" placeholder="Item..."/>
+			<input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}/>
 			<button>Add</button>
 		</form>
 	);
