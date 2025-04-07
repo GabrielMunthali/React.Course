f9aa81c37fd6889949b8d141b8d5bcbfc4578710
Gabriel Munthali <gabmunthali@gmail.com>
2025-04-07 11:25:39 +0200

    80. Thinking about state and lifting state up

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -21,11 +21,17 @@ const initialItems = [{
 ];
 
 export default function App() {
+	const [items, setItems] = useState([]);
+
+	function handleAddItems(item) {
+		setItems((items) => [...items, item]);
+	}
+
 	return (
 		<div className="app">
 			<Logo/>
-			<Form/>
-			<PackingList/>
+			<Form onAddItems = {handleAddItems}/>
+			<PackingList items = {items}/>
 			<Stats/>
 		</div>
 	);
@@ -35,7 +41,7 @@ function Logo() {
 		return <h1>🌴 Far Away 💼</h1>;
 	}
 
-function Form() {
+function Form({onAddItems}) {
 
 	const [description, setDescription] = useState("");
 	const [quantity, setQuantity] = useState(5);
@@ -47,6 +53,8 @@ function Form() {
 
 		const newItem = {description, quantity, packed: false, id: Date.now()};
 		console.log(newItem);
+
+		onAddItems(newItem);
 
 		setDescription("");
 		setQuantity(1);
@@ -66,11 +74,11 @@ function Form() {
 	);
 }
 
-function PackingList() {
+function PackingList({items}) {
 	return (
 		<div className="list">
 			<ul >
-				{initialItems.map((item) => (<Item item={item} key={item.id}/>))}
+				{items.map((item) => (<Item item={item} key={item.id}/>))}
 			</ul>
 		</div>
 	);
