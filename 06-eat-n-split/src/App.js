4fd3a91e6efb6795bda8b1612a81392263f10842
Gabriel Munthali <gabmunthali@gmail.com>
2025-04-24 20:59:01 +0200

    97. Building the static app : forms

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -23,7 +23,11 @@ export default function App() {
 		<div className="app">
 			<div className="sidebar">
 				<FriendList/>
+				<FormAddFriend/>
+				<Button>Add friend</Button>
 			</div>
+
+			<FormSplitBill/>
 		</div>
 	);
 }
@@ -64,7 +68,52 @@ function Friend({friend}) {
 				</p>
 			)}
 
-			<button className="button">Select</button>
+			<Button>Select</Button>
 		</li>
+	);
+}
+
+function Button({children}) {
+	return(
+		<button className="button">{children}</button>
+	);
+}
+
+function FormAddFriend() {
+	return(
+		<form className="form-add-friend">
+			<label>🧑🏾‍🤝‍🧑🏾 Friend name </label>
+			<input type="text" ></input>
+			
+			<label>🌇 Image URL</label>
+			<input type="text"></input>
+
+			<Button>Add</Button>
+		</form>
+	);
+}
+
+function FormSplitBill() {
+	return(
+		<form className="form-split-bill">
+			<h2>Split a bill with X</h2>
+
+			<label>💰 Bill value</label>
+			<input type="text"></input>
+
+			<label>🧍🏾‍♀️ Your expense</label>
+			<input type="text"></input>
+
+			<label>🧑🏾‍🤝‍🧑🏾 Xs expense</label>
+			<input type="text" disabled></input>
+
+			<label>🤑 Who is paying the bill</label>
+			<select>
+				<option value="user">You</option>
+				<option value="friend">X</option>
+			</select>
+
+			<Button>Split Bill</Button>
+		</form>
 	);
 }
